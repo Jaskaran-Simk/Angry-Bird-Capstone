4809cc38098cda4581e71156b162c919ff580abc
jaskaran <rashankaur867@gmail.com>
2020-09-02 17:01:22 +0530

    hi

--- sketch.js
+++ sketch.js
@@ -91,10 +91,13 @@ function mouseReleased(){
     slingshot.fly();
     gameState = "launched";
 }
-
+ 
 function keyPressed(){
-    if(keyCode === 32){
+    if(keyCode === 32 && bird.body.speed <1){
+        Matter.Body.setPosition(bird.body,{x : 200 , y : 50})
+        Matter.Body.setAngle(bird.body,0)
        slingshot.attach(bird.body);
+       bird.trajectory = []
     }
 }
 
@@ -105,7 +108,7 @@ async function getBackgroundImg(){
     var datetime = responseJSON.datetime;
     var hour = datetime.slice(11,13);
     
-    if(hour>=0600 && hour<=1900){
+    if(hour>=6 && hour<=19){
         bg = "sprites/bg1.png";
     }
     else{
